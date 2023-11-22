2788fa86a3fb66b7ab803c5602875e5e466c59b0
Arijit Bose <arijit.bose@indusnet.co.in>
2023-11-22 20:19:55 +0530

    Pod initiation

---
+++ IntaClone/Controller/CoreTabs/CameraViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CameraViewController.swift
+//  IntaClone
+//
+//  Created by Arijit Bose on 22/11/23.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ IntaClone/Controller/CoreTabs/ExploreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreViewController.swift
+//  IntaClone
+//
+//  Created by Arijit Bose on 22/11/23.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
